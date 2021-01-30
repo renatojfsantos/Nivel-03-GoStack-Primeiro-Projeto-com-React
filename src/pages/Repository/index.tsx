0569b00686321fa8d116909901ab379b6e1f15d4
Renato Santos <renato_jfs@hotmail.com>
2021-01-30 01:32:52 -0300

    Storage, Navegação entre rotas e estilização

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,69 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <ul>
+            <img
+              src="https://avatars.githubusercontent.com/u/67006002?s=460&u=9a2ccd94aa661b28bc8cb1a2e6da0217f30b7dad&v=4"
+              alt="Renato Santos"
+            />
+            <div>
+              <strong>rocketseat/unform</strong>
+              <p>descrição do repositório</p>
+            </div>
+          </ul>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="dsfsdfsdfs">
+          <div>
+            <strong>dfsdfdsfsfds</strong>
+            <p>dsdfdfdsfds</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
